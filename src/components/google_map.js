ad77a3af9e0379f74a49207814b7bf1552b38135
Radicevic <branko.radicevic@gtech.com>
2017-04-05 18:17:13 +0200

    add google react component

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
-class GoogleMap extends Component {
-    componentDidMount() {
-        new google.maps.Map(this.refs.map, {
-            zoom: 12,
-            center: {
-                lat: this.props.lat,
-                lng: this.props.lon
-            }
-        });
-    }
+
+class Map extends Component {
+    // componentDidMount() {
+    //     new google.maps.Map(this.refs.map, {
+    //         zoom: 12,
+    //         center: {
+    //             lat: this.props.lat,
+    //             lng: this.props.lon
+    //         }
+    //     });
+    // }
+
+    // render() {
+    //     return (
+    //         <div ref="map" />
+    //     );
+    // }
 
     render() {
         return (
-            <div ref="map" />
+            <GoogleMapLoader
+                containerElement={<div style={{ height: '100%' }} />}
+                googleMapElement={
+                    <GoogleMap defaultZoom={12} defaultCenter={{lat: this.props.lat, lng: this.props.lon}} />
+                }
+            />
         );
     }
 }
 
-export default GoogleMap;
+export default Map;
